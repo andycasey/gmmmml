d3ae9937877bf674b86df4c998d1c8f7efe3fbf9
Andy Casey <andycasey@gmail.com>
2018-03-14 14:13:30 +1100

    fix upload script

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -5,7 +5,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan pdf
   git rm -rf .
-  git add -f ms.pdf
+  git add -f article/ms.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
   git push -q -f https://andycasey:$GITHUB_API_KEY@github.com/andycasey/gmmmml pdf
 fi
