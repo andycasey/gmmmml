d5742b229643548e622d02164145c984e9a02e65
Andy Casey <andycasey@gmail.com>
2018-03-19 17:23:38 +1100

    moo6  is good approximation for LL

--- hack_nll.py
+++ hack_nll.py
@@ -32,7 +32,86 @@ def _approximate_log_gaussian_prob(X, weights, precision_cholesky, covariance_ty
     #    y = np.dot(X, prec_chol) - np.dot(mu, prec_chol)
     #    log_prob[:, k] = np.sum(np.square(y), axis=1)
 
-    return -0.5 * (n_features * np.log(2 * np.pi) + log_prob) + log_det
+    moo =  -0.5 * (n_features * np.log(2 * np.pi) + log_prob) + log_det
+    raise a
+
+
+def _approximate_log_likelihood(X, weights, precision_cholesky, covariance_type="full"):
+    n_samples, n_features = X.shape
+    #n_components, _ = means.shape
+    n_components = weights.size
+    # det(precision_chol) is half of det(precision)
+    log_det = mixture._compute_log_det_cholesky(
+        precision_cholesky, covariance_type, n_features)
+
+    
+    D = 10.0
+    scale = np.ptp(X, axis=0)/2.0
+
+    log_prob = np.ones((n_samples, n_components)) * D * np.mean(scale**2)
+
+    N = X.shape[0]
+    Sk = 0
+    for k, weight in enumerate(weights):
+        Nk = int(np.round(weight * N))
+        log_prob[Sk:Sk + Nk, k] = D
+        Sk += Nk
+
+    #for k, (mu, prec_chol) in enumerate(zip(means, precision_cholesky)):
+    #    y = np.dot(X, prec_chol) - np.dot(mu, prec_chol)
+    #    log_prob[:, k] = np.sum(np.square(y), axis=1)
+
+    moo =  -0.5 * (n_features * np.log(2 * np.pi) + log_prob) + log_det 
+    moo = log_det + np.log(weights) - 0.5 * n_features * np.log(2 * np.pi) \
+        - 0.5 * log_prob
+
+    scalar = np.exp(-0.5 * D * np.log(2*np.pi))
+
+    det = np.exp(log_det)
+    moo2 = scalar * det * weights * np.exp(-0.5 * log_prob)
+
+    # when a log_prob entry indicates it is correct, it is 1
+    # if not, it is some huge value
+
+    # np.exp(-0.5) = 0.6065
+    # np.exp(-10000) = 0
+
+    # therefore,...
+
+    prob = np.zeros_like(log_prob)
+    Sk = 0
+    for k, weight in enumerate(weights):
+        Nk = int(np.round(weight * N))
+        prob[Sk:Sk + Nk, k] = np.exp(-0.5 * D)
+        Sk += Nk
+
+    A = det * weights
+    B = scalar * prob
+    
+    moo3 = np.dot(A, B.T)
+
+    # correct: LL = np.sum(np.log(moo3))
+
+    moo4 = det * weights * scalar * np.exp(-0.5 * D) #* (N * weights)
+
+    # correct: LL = np.sum(N * weights * np.log(moo4))
+
+    moo5 = N * np.sum(weights * np.log(det * weights)) - 0.5 * N * D * (np.log(2*np.pi) + 1)
+
+    # correct: LL = moo5
+
+    moo6 = N * np.sum(weights * np.log(weights)) \
+         + N * np.sum(weights * log_det) \
+         - 0.5 * N * D * (np.log(2 * np.pi) + 1)
+
+
+    # Compute the log of the sum of expoentials of input elements.
+
+
+
+    raise a
+
+
 
 
 
@@ -64,6 +143,10 @@ for i in range(10):
     weighted_log_prob = np.log(weight) + bar
 
     log_likelihood = scipy.misc.logsumexp(weighted_log_prob, axis=1)
+
+    foo = _approximate_log_likelihood(y, weight, precision_cholesky)
+
+    raise a
 
     foo = _approximate_log_gaussian_prob(y, weight, precision_cholesky)
 
