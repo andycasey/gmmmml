4414ce45c9c53b9afa69ef2e28bdf0db0fe1d37c
Andy Casey <andycasey@gmail.com>
2018-09-12 19:05:09 +1000

    fix travis build

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -x
 
-# SSL workaround
-mkdir -p ~/.config/Tectonic
-cp .ci/tectonic.config.toml ~/.config/Tectonic/config.toml
-
 # Conda install
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
@@ -12,5 +8,7 @@ hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
-conda install -c conda-forge -c pkgw-forge tectonic
+conda config --add channels conda-forge
+conda update --all
+conda install tectonic
 tectonic --help
--- .ci/upload.sh
+++ .ci/upload.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -x
 
 # Push to GitHub
-if [ -n "$GITHUB_API_KEY" ]; then
-  cd $TRAVIS_BUILD_DIR
-  git checkout --orphan pdf
-  git rm -rf .
-  git add -f article/ms.pdf
-  git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
-  git push -q -f https://andycasey:$GITHUB_API_KEY@github.com/andycasey/gmmmml pdf
-fi
+cd $TRAVIS_BUILD_DIR
+git checkout --orphan pdf
+git rm -rf .
+git add -f article/ms.pdf
+git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
+git push -q -f https://andycasey:$GITHUB_API_KEY@github.com/andycasey/gmmmml pdf
